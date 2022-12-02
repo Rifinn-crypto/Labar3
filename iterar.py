a10430bd56b0247888e46b244497e3e868d2acd4
Nikita Raven <esh2015@list.ru>
2022-12-02 20:54:40 +0400

    class DateIter

--- iterar.py
+++ iterar.py
@@ -19,5 +19,21 @@ import os
                     return self.df.loc[self.counter - 1]["Day"], self.df.loc[self.counter - 1]["Exchange rate"]
         raise FileNotFoundError
 
+ class DateIteratorXY:
+
+    def __init__(self):
+
+        self.counter = 0
+        self.xf = pd.read_csv("C:/Users/esh20/PycharmProjects/Lab2/1/X.csv")
+        self.yf = pd.read_csv("C:/Users/esh20/PycharmProjects/Lab2/1/Y.csv")
+
+    def __next__(self) -> tuple:
+        if self.counter == self.xf.shape[0]:
+            raise StopIteration
+
+        elif self.counter < self.xf.shape[0]:
+            self.counter += 1
+            return self.xf.loc[self.counter - 1]["Day"], self.yf.loc[self.counter - 1]["Exchange rate"]
+
 
 
