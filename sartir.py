146dbf653515aece0482c99d743f8b4ae80bae73
Nikita Raven <esh2015@list.ru>
2022-12-02 21:22:40 +0400

    Function formatted

--- sartir.py
+++ sartir.py
@@ -1,2 +1,13 @@
 import pandas as pd
 import autopep8
+
+
+def formatted_file(input_file: str) -> pd.DataFrame:
+    df = pd.read_csv(input_file)
+
+    df["Day"] = pd.to_datetime(df.Day, format="%Y-%m-%d")
+    df["Day1"] = df["Day"].dt.date
+    df["Year"] = df["Day"].dt.year
+    df["Week"] = df["Day"].dt.isocalendar().week
+    return df
+
