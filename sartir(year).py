c6dc287d7cb06a5e32ea100961129b4d263d1e5c
Nikita Raven <esh2015@list.ru>
2022-12-03 13:02:11 +0400

    Function formated file

--- sartir(year).py
+++ sartir(year).py
@@ -1,2 +1,11 @@
 import pandas as pd
 from typing import NoReturn
+
+
+def formatted_file(input_file: str) -> pd.DataFrame:
+    df = pd.read_csv(input_file)
+
+    df["Day"] = pd.to_datetime(df.Day, format="%Y-%m-%d")
+    df["Day1"] = df["Day"].dt.date
+    df["Year"] = df["Day"].dt.year
+    return df
