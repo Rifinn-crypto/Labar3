6de5cfdc3ae1f85a222fb49d38d350ddd4c9901a
Nikita Raven <esh2015@list.ru>
2022-12-03 13:05:08 +0400

    Function formated file

--- sartir(year).py
+++ sartir(year).py
@@ -9,3 +9,13 @@ def formatted_file(input_file: str) -> pd.DataFrame:
     df["Day1"] = df["Day"].dt.date
     df["Year"] = df["Day"].dt.year
     return df
+
+
+def write_to_file(input_file: str, year: int) -> NoReturn:
+    df = formatted_file(input_file)
+
+    df = df[df["Year"] == year]
+    data = str(df["Day1"].iloc[0]).replace("-", "") + "_" + str(df["Day1"].iloc[df.shape[0] - 1]).replace("-", "")
+    del df["Year"]
+    del df["Day1"]
+    df.to_csv(data + ".csv", index=False)
