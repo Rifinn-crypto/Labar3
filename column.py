22be597c10e042cadb1f278cca26eb3e41f979f2
Nikita Raven <esh2015@list.ru>
2022-12-04 12:49:01 +0400

    end column

--- column.py
+++ column.py
@@ -6,9 +6,10 @@ def write_to_file(input_file: str, file_for_x: str, file_for_y: str) -> None:
     df["Day"].to_csv(file_for_x, index=False)
     df["Exchange rate"].to_csv(file_for_y, index=False)
 
- if __name__ == "__main__":
-     my_file_for_dates = "X.csv"
-     my_file_for_data = "Y.csv"
-     file = "C:/Users/esh20/Desktop/dataset.csv"
 
-     write_to_file(file, my_file_for_dates, my_file_for_data)
+if __name__ == "__main__":
+    my_file_for_dates = "X.csv"
+    my_file_for_data = "Y.csv"
+    file = "C:/Users/esh20/Desktop/dataset.csv"
+
+    write_to_file(file, my_file_for_dates, my_file_for_data)
